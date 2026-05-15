18314be6e86ee5d5bb1467a9f5e84a13b66a61a4
kaji <dafe00842202@outlook.jp>
2026-05-15 14:17:47 +0900

    chore: 全課題フォルダを正しく追加

--- ST31_kadai02_IH13A_kaji_compose/app/src/main/java/com/example/st31_kadai02_ih13a_kaji_compose/MainActivity.kt
+++ ST31_kadai02_IH13A_kaji_compose/app/src/main/java/com/example/st31_kadai02_ih13a_kaji_compose/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.st31_kadai02_ih13a_kaji_compose.ui.theme.ST31_kadai02_IH13A_kaji_composeTheme
@@ -19,13 +21,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ST31_kadai02_IH13A_kaji_composeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App()
             }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun App() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Greeting(
+                name = "Android",
+                modifier = Modifier
+                    .padding(innerPadding)
+            )
         }
     }
 }
@@ -36,12 +54,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ST31_kadai02_IH13A_kaji_composeTheme {
-        Greeting("Android")
-    }
 }
