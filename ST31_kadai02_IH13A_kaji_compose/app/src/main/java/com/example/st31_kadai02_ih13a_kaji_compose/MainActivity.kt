b19da34beaaeadb3b20dcb5620e599c809c501ed
kaji <dafe00842202@outlook.jp>
2026-05-15 14:10:06 +0900

    update

---
+++ ST31_kadai02_IH13A_kaji_compose/app/src/main/java/com/example/st31_kadai02_ih13a_kaji_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.st31_kadai02_ih13a_kaji_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.st31_kadai02_ih13a_kaji_compose.ui.theme.ST31_kadai02_IH13A_kaji_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ST31_kadai02_IH13A_kaji_composeTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ST31_kadai02_IH13A_kaji_composeTheme {
+        Greeting("Android")
+    }
+}
