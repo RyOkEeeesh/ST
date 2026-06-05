040183e2f41b9f4071b62e0781d7040637bb16a1
kaji <dafe00842202@outlook.jp>
2026-06-05 11:32:40 +0900

    update

--- ST31_kadai03_IGH13A_kaji/app/src/main/java/com/example/st31_kadai03_igh13a_kaji/MainActivity.kt
+++ ST31_kadai03_IGH13A_kaji/app/src/main/java/com/example/st31_kadai03_igh13a_kaji/MainActivity.kt
@@ -6,16 +6,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.st31_kadai03_igh13a_kaji.ui.theme.ST31_kadai03_IGH13A_kajiTheme
 import com.example.st31_kadai03_igh13a_kaji.R
 
@@ -31,18 +47,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
+    var result by remember { mutableStateOf("") }
+
+    val results: Array<String> = arrayOf(
+        "よき",
+        "まあよき",
+        "まあまあ",
+        "うんー",
+        "びみょ",
+        "あんま"
+    )
+
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .clipToBounds()
+                .clipToBounds(),
+            verticalArrangement = Arrangement.spacedBy(
+                16.dp,
+                Alignment.CenterVertically
+            ),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.index),
-                contentDescription = "写真"
+            Text(
+                text = "画像をクリックしてね",
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
+            Button(
+                colors = ButtonDefaults.buttonColors( containerColor = Color.Transparent ),
+                onClick = { result = results.random() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.index),
+                    contentDescription = "写真",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth,
+                )
+            }
+            Text(
+                text = result,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
             )
         }
     }
