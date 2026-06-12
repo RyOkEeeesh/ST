f7f7527deb6574183542ed72c1a16bbdd094e88c
kaji <dafe00842202@outlook.jp>
2026-06-12 12:28:07 +0900

    commit

--- kadai3_option_IH13A_kaji/app/src/main/java/com/example/kadai3_option_ih13a_kaji/MainActivity.kt
+++ kadai3_option_IH13A_kaji/app/src/main/java/com/example/kadai3_option_ih13a_kaji/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.st31_kadai03_igh13a_kaji
+package com.example.kadai3_option_ih13a_kaji
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,51 +11,52 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.kadai3_option_ih13a_kaji.ui.theme.Kadai3_option_IH13A_kajiTheme
+import kotlin.math.min
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            Kadai3_option_IH13A_kajiTheme {
+                    App()
+            }
         }
     }
 }
 
 @Composable
 fun App() {
-    var result by remember { mutableStateOf("") }
-
-    val results: Array<String> = arrayOf(
-        "よき",
-        "まあよき",
-        "まあまあ",
-        "うんー",
-        "びみょ",
-        "あんま"
+    val max = 20
+    val imgs = listOf(
+        R.drawable.f0,
+        R.drawable.f1,
+        R.drawable.f2,
+        R.drawable.f3,
+        R.drawable.f4,
+        R.drawable.f5,
     )
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-    ) { innerPadding ->
+    var count by remember { mutableIntStateOf(0) }
+
+    val img = imgs[min(count * imgs.size / max, imgs.size - 1)]
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -65,30 +66,19 @@ fun App() {
                 16.dp,
                 Alignment.CenterVertically
             ),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "画像をクリックしてね",
+            Image(
+                painter = painterResource(id = img),
+                contentDescription = "写真",
                 modifier = Modifier.fillMaxWidth(),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
+                contentScale = ContentScale.FillWidth,
             )
             Button(
-                colors = ButtonDefaults.buttonColors( containerColor = Color.Transparent ),
-                onClick = { result = results.random() }
+                onClick = { count++ }
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.index),
-                    contentDescription = "写真",
-                    modifier = Modifier.fillMaxWidth(),
-                    contentScale = ContentScale.FillWidth,
-                )
+                Text("水をあげる")
             }
-            Text(
-                text = result,
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
-            )
         }
     }
 }
@@ -96,5 +86,5 @@ fun App() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun AppPreview() {
-   App()
+    App()
 }
